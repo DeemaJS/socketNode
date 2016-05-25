d7bb321b22c60b71b6289630d378d9ea247f1845
DeemaJS <deeema@outlook.com>
2016-05-25 15:41:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
+var fs = require('fs');
+var url = require('url');
+var http = require('http');
+var path = require('path');
+var express = require('express');
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+var root = __dirname;
+app.use(function (req, res, next) {
+req.on('static', function () {
+var file = url.parse(req.url).pathname;
+var mode = 'stylesheet';
+if (file[file.length - 1] == '/') {
+file += 'index.html';
+mode = 'reload';
+}
 
